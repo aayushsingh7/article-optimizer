3f87ecfb15cf5024a8d3c36ed8580af165495f51
aayushsingh7 <97450185+aayushsingh7@users.noreply.github.com>
2026-01-01 00:29:56 +0530

    chore: add free-hosting loading notice

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Loader = ({text}) => {
   return (
-    <div className='h-[500px] w-full flex items-center justify-center flex-col'>
+    <div className='h-[400px] w-full flex items-center justify-center flex-col px-10'>
          <div className='loader'/>
          <span className='mt-[20px] inline-block font-bold text-lg'>{text}</span>
+         <span className='font-bold text-gray-400 text-lg text-center'>Render.com may take up to 50 seconds to respond (free hosting).</span>
     </div>
   )
 }
